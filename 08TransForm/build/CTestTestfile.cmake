1ace48b54ed8cd8f0f4962c3eb33d8e62a848ac9
John <2502785143@qq.com>
2023-10-23 20:24:28 +0800

    实验ignore

---
+++ 08TransForm/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/john/Elegent/08TransForm/src
+# Build directory: /home/john/Elegent/08TransForm/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("learning_tf")
---
+++ 08TransForm/build/learning_tf/CMakeFiles/turtle_tf_broadcaster.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/john/Elegent/08TransForm/devel/lib/learning_tf/turtle_tf_broadcaster"
+  "/home/john/Elegent/08TransForm/devel/lib/learning_tf/turtle_tf_broadcaster.pdb"
+  "CMakeFiles/turtle_tf_broadcaster.dir/src/turtle_tf_broadcaster.cpp.o"
+  "CMakeFiles/turtle_tf_broadcaster.dir/src/turtle_tf_broadcaster.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/turtle_tf_broadcaster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
