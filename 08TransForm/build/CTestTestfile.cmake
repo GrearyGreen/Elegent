9876fdc30af33e4610132b5975daaedae14922a0
John <2502785143@qq.com>
2023-10-23 20:26:28 +0800

    重来

--- 08TransForm/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/john/Elegent/08TransForm/src
-# Build directory: /home/john/Elegent/08TransForm/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("learning_tf")
